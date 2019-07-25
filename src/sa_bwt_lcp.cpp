2ed9fcb9799ca51561c19dbcbf4d4eba60a9ad0b
nishimoto <a32b16c4@gmail.com>
2019-07-25 23:20:08 +0900

    minor fix

--- src/sa_bwt_lcp.cpp
+++ src/sa_bwt_lcp.cpp
@@ -4,26 +4,23 @@
 #include <vector>
 #include <iostream>
 
-
-
 namespace stool
 {
 
-template <typename CHAR,typename INDEX>
+template <typename CHAR, typename INDEX>
 std::vector<INDEX> constructSA(std::vector<CHAR> &text)
 {
     std::vector<INDEX> sa;
-    
+
     uint64_t n = text.size();
     sa.resize(n);
 
     divsufsort64((const unsigned char *)&text[0], (int64_t *)&sa[0], n);
     return sa;
-    
 }
-template std::vector<uint64_t> stool::constructSA<uint8_t,uint64_t>(std::vector<uint8_t>&);
+template std::vector<uint64_t> constructSA<uint8_t, uint64_t>(std::vector<uint8_t> &);
 
-template <typename CHAR,typename INDEX>
+template <typename CHAR, typename INDEX>
 std::vector<INDEX> constructISA(std::vector<CHAR> &text, std::vector<INDEX> &sa)
 {
     std::vector<INDEX> isa;
@@ -36,7 +33,7 @@ std::vector<INDEX> constructISA(std::vector<CHAR> &text, std::vector<INDEX> &sa)
     }
     return isa;
 }
-template std::vector<uint64_t> stool::constructISA<uint8_t,uint64_t>(std::vector<uint8_t>&, std::vector<uint64_t>&);
+template std::vector<uint64_t> constructISA<uint8_t, uint64_t>(std::vector<uint8_t> &, std::vector<uint64_t> &);
 
 /*
 void constructSA(string &text, vector<uint64_t> &sa, vector<uint64_t> &isa)
@@ -59,7 +56,7 @@ void constructSA(string &text, vector<uint64_t> &sa, vector<uint64_t> &isa)
     std::cout << "[END]" << std::endl;
 }
 */
-template <typename CHAR,typename INDEX>
+template <typename CHAR, typename INDEX>
 std::vector<INDEX> constructLCP(std::vector<CHAR> &text, std::vector<INDEX> &sa, std::vector<INDEX> &isa)
 {
     std::vector<INDEX> lcp;
@@ -89,35 +86,39 @@ std::vector<INDEX> constructLCP(std::vector<CHAR> &text, std::vector<INDEX> &sa,
         if (k > 0)
             k--;
     }
-    std::cout << "[END]"<< std::endl;
+    std::cout << "[END]" << std::endl;
     return lcp;
 }
 template std::vector<uint64_t> constructLCP(std::vector<uint8_t> &, std::vector<uint64_t> &, std::vector<uint64_t> &);
 
-template <typename CHAR,typename INDEX>
+template <typename CHAR, typename INDEX>
 std::vector<INDEX> constructLCP(std::vector<CHAR> &text, std::vector<INDEX> &sa)
 {
-    std::vector<INDEX> isa = stool::constructISA<CHAR,INDEX>(text,sa);
-    return constructLCP<CHAR,INDEX>(text,sa,isa);
+    std::vector<INDEX> isa = stool::constructISA<CHAR, INDEX>(text, sa);
+    return constructLCP<CHAR, INDEX>(text, sa, isa);
     //lcp.resize(text.size(), 0);
-
 }
 template std::vector<uint64_t> constructLCP(std::vector<uint8_t> &, std::vector<uint64_t> &);
 
-template <typename CHAR,typename INDEX>
-std::vector<CHAR> constructBWT(std::vector<CHAR> &text, std::vector<INDEX> &sa){    
+template <typename CHAR, typename INDEX>
+std::vector<CHAR> constructBWT(std::vector<CHAR> &text, std::vector<INDEX> &sa)
+{
     std::vector<CHAR> bwt;
     bwt.resize(text.size());
     INDEX n = text.size();
-    for(INDEX i=0;i<text.size();i++){
-        if(sa[i] == 0){
-            bwt[i] = text[n-1];
-        }else{
-            bwt[i] = text[sa[i]-1];
+    for (INDEX i = 0; i < text.size(); i++)
+    {
+        if (sa[i] == 0)
+        {
+            bwt[i] = text[n - 1];
+        }
+        else
+        {
+            bwt[i] = text[sa[i] - 1];
         }
     }
     return bwt;
 }
-template std::vector<uint8_t> constructBWT(std::vector<uint8_t> &, std::vector<uint64_t> &); 
-template std::vector<char> constructBWT(std::vector<char> &, std::vector<uint64_t> &); 
+template std::vector<uint8_t> constructBWT(std::vector<uint8_t> &, std::vector<uint64_t> &);
+template std::vector<char> constructBWT(std::vector<char> &, std::vector<uint64_t> &);
 } // namespace stool
--- src/sa_bwt_lcp.hpp
+++ src/sa_bwt_lcp.hpp
@@ -4,7 +4,7 @@
 #include <string>
 #include <iostream>
 #include <vector>
-
+#include <limits>
 
 namespace stool
 {
