890496583ef30c2496cdfae8f4e763c2f090a2b2
Nishimoto <a32b16c4@gmail.com>
2019-07-30 14:26:21 +0900

    io

--- src/io.hpp
+++ src/io.hpp
@@ -10,8 +10,6 @@
 namespace stool
 {
 
-
-
 template <typename T>
 std::vector<T> load(std::string filename)
 {
@@ -40,6 +38,48 @@ std::vector<T> load(std::string filename)
 std::vector<char> load_text(std::string filename);
 std::vector<uint8_t> load_text_from_file(std::string filename, bool appendSpecialCharacter = true);
 
+template <typename T>
+static bool write_vector(std::string &filename, std::vector<T> &text)
+{
+	std::cout << "Writing: " << filename << std::endl;
+	std::ofstream out(filename, std::ios::out | std::ios::binary);
+	//write<T>(out, text);
+
+	if (!out)
+		return 1;
+	uint64_t size = text.size();
+	out.write(reinterpret_cast<const char *>(&size ), sizeof(uint64_t));
+	out.write(reinterpret_cast<const char *>(&text[0]), text.size() * sizeof(T));
+
+	out.close();
+	return true;
+}
+
+/*
+template <typename T>
+void load_vector(std::string filename, std::string &output)
+{
+	std::cout << "Loading: " << filename << std::endl;
+	std::ifstream inputStream;
+	inputStream.open(filename, std::ios::binary);
+
+	if (!file)
+	{
+		std::cerr << "error reading file " << std::endl;
+		return false;
+	}
+	file.seekg(0, std::ios::end);
+	uint64_t n = (unsigned long)file.tellg();
+	file.seekg(0, std::ios::beg);
+
+	output.resize(n / sizeof(char));
+
+	file.read((char *)&(output)[0], n);
+	file.close();
+	file.clear();
+
+}
+*/
 
 /*
 std::vector<int64_t> construct_sa(std::vector<char> &text)
@@ -55,5 +95,3 @@ std::vector<int64_t> construct_sa(std::vector<char> &text)
 */
 
 } // namespace stool
-
-
