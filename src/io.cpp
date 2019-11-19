39523448a8e9ba516c16bacb33af14e6cb7352ba
Nishimoto <a32b16c4@gmail.com>
2019-11-19 11:54:03 +0900

    added std::string load_string_from_file(std::string filename, bool appendSpecialCharacter){

--- src/io.cpp
+++ src/io.cpp
@@ -97,5 +97,33 @@ std::vector<char> load_char_vec_from_file(std::string filename, bool appendSpeci
 	}
 	return vec;
 }
+std::string load_string_from_file(std::string filename, bool appendSpecialCharacter){
+	std::ifstream stream;
+	stream.open(filename, std::ios::binary);
 
+	std::string vec;
+
+	if (!stream)
+	{
+		std::cerr << "error reading file " << std::endl;
+		throw - 1;
+	}
+	uint64_t len;
+	stream.seekg(0, std::ios::end);
+	uint64_t n = (unsigned long)stream.tellg();
+	stream.seekg(0, std::ios::beg);
+	len = n / sizeof(uint8_t);
+
+	vec.resize(len + (appendSpecialCharacter ? 1 : 0 ), 0);
+	stream.read((char *)&(vec)[0], len * sizeof(char));
+
+	for (uint64_t i = 0; i < len; i++)
+	{
+		if (vec[i] == 0)
+		{
+			throw std::logic_error("The input text must not contain '0' character!");
+		}
+	}
+	return vec;
+}
 }
--- src/io.hpp
+++ src/io.hpp
@@ -37,7 +37,7 @@ std::vector<char> load_text(std::string filename);
 std::vector<uint8_t> load_text_from_file(std::string filename, bool appendSpecialCharacter = true);
 
 std::vector<char> load_char_vec_from_file(std::string filename, bool appendSpecialCharacter = true);
-
+std::string load_string_from_file(std::string filename, bool appendSpecialCharacter = true);
 
 template <typename T>
 bool write_vector(std::string &filename, std::vector<T> &text, bool addLengthInfo)
