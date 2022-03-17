5947b61cffab53b4d868c8f93fa999b8d1ee4873
takaaki.nishimoto <takaaki.nishimoto@riken.jp>
2022-03-17 14:09:03 +0900

    sort

--- include/sort.hpp
+++ include/sort.hpp
@@ -12,8 +12,23 @@ namespace stool
     class SortString
     {
     public:
-        static bool compare(const std::vector<uint8_t> &str1, const std::vector<uint8_t> &str2)
+        static std::pair<uint64_t, int8_t> lcp_and_compare(const std::vector<uint8_t> &str1, const std::vector<uint8_t> &str2)
         {
+            uint64_t lcp = SortString::lcp(str1, str2);
+            int8_t com = 0;
+            if(lcp < str1.size() && lcp < str2.size()){
+                com = str1[lcp] <  str2[lcp] ? -1 : 1;
+            }else{
+                if(str1.size() < str2.size()){
+                    com = -1;
+                }else if(str1.size() > str2.size()){
+                    com = 1;
+                }else{
+                    com = 0;
+                }
+            }
+            return std::pair<uint64_t, int8_t>(lcp, com);
+            /*
             uint64_t min = std::min(str1.size(), str2.size());
             for (uint64_t i = 0; i < min; i++)
             {
@@ -27,6 +42,7 @@ namespace stool
                 return str1.size() < str2.size();
             }
             return false;
+            */
         }
 
         static uint64_t lcp(const std::vector<uint8_t> &str1, const std::vector<uint8_t> &str2)
