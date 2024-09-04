04cd7f6c93c43ffbb4ba0a6de35cb1a27cd5f42a
Nishimoto <a32b16c4@gmail.com>
2024-09-04 19:47:29 +0900

    mem

--- include/debug.hpp
+++ include/debug.hpp
@@ -11,27 +11,8 @@
 
 #include "./print.hpp"
 
-#ifdef __linux__
-#include <malloc.h>
-#elif defined(__APPLE__)
-#include <stdlib.h>
-#include <malloc/malloc.h>
-#endif
-
 namespace stool
 {
-	void print_memory_usage()
-	{
-#ifdef __linux__
-		struct mallinfo mi = mallinfo();
-		std::cout << "Total allocated space: " << mi.uordblks << " bytes" << std::endl;
-#elif defined(__APPLE__)
-		malloc_zone_t *zone = malloc_default_zone();
-		malloc_statistics_t stats;
-		malloc_zone_statistics(zone, &stats);
-		std::cout << "Total allocated space: " << (stats.size_in_use / 1000) << "KB" << std::endl;
-#endif
-	}
 
 	/*
 	std::string CreateRandomString(int len, int alphabetSize)
