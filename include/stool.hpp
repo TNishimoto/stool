413c6408eb3b4bde6bf0abfbee35dc60c15f1545
Nishimoto <a32b16c4@gmail.com>
2024-11-19 15:01:13 +0900

    stool.hpp

---
+++ include/stool.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "./specialized_collection/elias_fano_vector.hpp"
+#include "./rlbwt/bwt_decompress.hpp"
+#include "./rlbwt/forward_sa.hpp"
+#include "./rlbwt/fpos_data_structure.hpp"
+#include "./rlbwt/rle.hpp"
+#include "./rlbwt/rle_wavelet_tree.hpp"
+
+
+
+
