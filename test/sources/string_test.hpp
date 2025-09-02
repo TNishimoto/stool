cd6b4113fae1f2538a86cca3f3b27e895d6bee12
Nishimoto <a32b16c4@gmail.com>
2025-09-02 19:38:57 +0900

    test

--- test/sources/string_test.hpp
+++ test/sources/string_test.hpp
@@ -9,32 +9,25 @@
 #include <bitset>
 #include "../../include/light_stool.hpp"
 
-namespace stool{
-    class StringTest{
-        public:
+namespace stool
+{
+    class StringTest
+    {
+    public:
         template <typename TEXT>
-        static void access_character_test(TEXT &test_text, const std::vector<uint8_t> &alphabet, uint64_t text_size, uint64_t number_of_access, uint64_t seed){
+        static void access_character_test(TEXT &test_text, const std::vector<uint8_t> &naive_text, uint64_t number_of_access, uint64_t seed)
+        {
             std::mt19937_64 mt64(seed);
             std::uniform_int_distribution<uint64_t> get_rand_uni_int(0, UINT64_MAX);
 
-            std::vector<uint8_t> _text = stool::UInt8VectorGenerator::create_random_sequence(text_size, alphabet, seed);
-
-            test_text.clear();
-            test_text.set_alphabet(alphabet);
-
-            stool::NaiveDynamicString naive_text;
-            for(char c: _text){
-                naive_text.push_back(c);
-            }
-            test_text.build_from_text(_text, alphabet);
-
-
-            for(uint64_t i = 0; i < number_of_access;i++){
+            for (uint64_t i = 0; i < number_of_access; i++)
+            {
                 uint64_t nth = get_rand_uni_int(mt64) % (naive_text.size());
-                uint8_t c = naive_text.text[nth];
+                uint8_t c = naive_text[nth];
                 uint8_t c2 = test_text[nth];
 
-                if(c != c2){
+                if (c != c2)
+                {
                     std::cout << "Error: c != c2" << std::endl;
                     throw std::runtime_error("Error: c != c2");
                 }
@@ -42,22 +35,111 @@ namespace stool{
         }
 
         template <typename TEXT>
-        static void access_character_test(TEXT &test_text, uint64_t max_text_size, uint64_t number_of_access, uint64_t seed, int message_paragraph = stool::Message::SHOW_MESSAGE)
+        static void access_character_test(uint64_t max_text_size, uint64_t number_of_access, uint64_t number_of_trials, 
+            std::function<void(TEXT &, const std::vector<uint8_t> &, const std::vector<uint8_t> &)> builder_function, uint64_t seed, int message_paragraph = stool::Message::SHOW_MESSAGE)
         {
             std::cout << stool::Message::get_paragraph_string(message_paragraph) << "Access Character Test: " << std::endl;
+            TEXT test_text;
             for (uint64_t type = 0; type <= stool::UInt8VectorGenerator::get_max_alphabet_type(); type++)
             {
-                std::cout << stool::Message::get_paragraph_string(message_paragraph + 1) << "Alphabet Type: " << type << std::flush;
+
                 uint64_t len = 2;
-                std::vector<uint8_t> chars = stool::UInt8VectorGenerator::create_alphabet(type);
+                std::vector<uint8_t> alphabet = stool::UInt8VectorGenerator::create_alphabet(type);
+                std::cout << stool::Message::get_paragraph_string(message_paragraph + 1) << "Alphabet Type: " << type << ", len = " << std::flush;
 
                 while (len < max_text_size)
                 {
-                    std::cout << "+" << std::flush;
-                    access_character_test(test_text, chars, len, number_of_access, seed++);
+                    std::cout << len << " " << std::flush;
+                    for(uint64_t i = 0; i < number_of_trials; i++){
+                        std::vector<uint8_t> _text = stool::UInt8VectorGenerator::create_random_sequence(len, alphabet, seed++);
+                        stool::NaiveDynamicString naive_text;
+                        for (char c : _text)
+                        {
+                            naive_text.push_back(c);
+                        }
+                        TEXT test_text;
+                        builder_function(test_text, _text, alphabet);
+
+                        access_character_test(test_text, _text, number_of_access, seed++);
+
+                    }
+    
                     len *= 2;
                 }
                 std::cout << std::endl;
+            }
+            std::cout << "[DONE]" << std::endl;
+        }
+
+        template <typename TEXT>
+        static void locate_query_test(TEXT &test_text, const std::vector<uint8_t> &original_text, uint64_t number_of_queries, uint64_t max_query_length, uint64_t seed, [[maybe_unused]]int message_paragraph = stool::Message::SHOW_MESSAGE)
+        {
+            std::mt19937_64 mt64(seed);
+            std::vector<uint64_t> sa = stool::StringFunctions::construct_naive_suffix_array(original_text);
+
+            for (uint64_t i = 0; i < number_of_queries; i++)
+            {
+                uint64_t pos = mt64() % original_text.size();
+                uint64_t pattern_length = mt64() % (original_text.size() - pos) + 1;
+
+                if (pattern_length > max_query_length)
+                {
+                    pattern_length = max_query_length;
+                }
+
+                std::vector<uint8_t> pattern = std::vector<uint8_t>(original_text.begin() + pos, original_text.begin() + pos + pattern_length);
+
+                std::vector<uint64_t> occurrences = stool::StringFunctionsOnSA::locate_query(original_text, pattern, sa);
+                std::vector<uint64_t> test_occurrences = test_text.locate_query(pattern);
+
+                std::sort(occurrences.begin(), occurrences.end());
+                std::sort(test_occurrences.begin(), test_occurrences.end());
+                try
+                {
+                    stool::EqualChecker::equal_check(occurrences, test_occurrences, "locate_query_test");
+                }
+                catch (const std::exception &e)
+                {
+                    std::string text_str = std::string(original_text.begin(), original_text.end());
+                    std::string pattern_str = std::string(pattern.begin(), pattern.end());
+
+                    std::cout << "text_str = " << text_str << std::endl;
+                    std::cout << "pattern_str = " << pattern_str << std::endl;
+
+                    std::cout << std::endl;
+
+                    stool::DebugPrinter::print_integers(occurrences, "occurrences");
+                    stool::DebugPrinter::print_integers(test_occurrences, "test_occurrences");
+                    throw std::runtime_error("ERROR");
+                    return;
+                }
+            }
+        };
+
+        template <typename TEST_TEXT>
+        static void locate_query_test(uint64_t max_len, uint64_t alphabet_type, [[maybe_unused]] uint64_t number_of_trials, uint64_t number_of_pattern_trials, 
+            std::function<void(TEST_TEXT&, const std::vector<uint8_t>&, const std::vector<uint8_t>&)> builder_function, uint64_t seed)
+        {
+            std::cout << "LOCATE_QUERY_TEST \t" << std::endl;
+            TEST_TEXT test_index;
+
+            for (uint64_t len = 4; len <= max_len; len *= 2)
+            {
+                std::cout << stool::Message::get_paragraph_string(2) << "len = " << len << ": " << std::flush;
+                auto alphabet = stool::UInt8VectorGenerator::create_alphabet(alphabet_type);
+
+                for(uint64_t i = 0; i < number_of_trials; i++){
+                    std::cout << "+" << std::flush;
+                    std::vector<uint8_t> text = stool::UInt8VectorGenerator::create_random_sequence(len, alphabet, seed++);
+                    builder_function(test_index, text, alphabet);
+    
+                    uint64_t max_query_length = len / 2;
+    
+                    locate_query_test(test_index, text, number_of_pattern_trials, max_query_length, seed);
+    
+                }
+                std::cout << std::endl;
+
             }
             std::cout << "[DONE]" << std::endl;
         }
--- test/sources/template/dynamic_string_test.hpp
+++ test/sources/template/dynamic_string_test.hpp
@@ -8,6 +8,7 @@
 #include <random>
 #include <bitset>
 #include "../../../include/light_stool.hpp"
+#include "../string_test.hpp"
 
 namespace stool
 {
@@ -15,14 +16,15 @@ namespace stool
     {
     public:
         template <typename TEXT>
-        static void insert_character_test(TEXT &test_text, const std::vector<uint8_t> &alphabet, uint64_t number_of_insertion, uint64_t seed)
+        static void insert_character_test(TEXT &test_text, stool::NaiveDynamicString &naive_text, const std::vector<uint8_t> &alphabet, uint64_t number_of_insertion, [[maybe_unused]] bool detailed_check, uint64_t seed)
         {
-
+            /*
             stool::NaiveDynamicString naive_text;
             test_text.clear();
             naive_text.clear();
+            */
 
-            test_text.set_alphabet(alphabet);
+            // test_text.set_alphabet(alphabet);
 
             std::mt19937_64 mt64(seed);
             std::uniform_int_distribution<uint64_t> get_rand_uni_int(0, number_of_insertion);
@@ -42,20 +44,34 @@ namespace stool
         }
 
         template <typename TEXT>
-        static void insert_character_test(TEXT &test_text, uint64_t number_of_insertion, uint64_t seed, int message_paragraph = stool::Message::SHOW_MESSAGE)
+        static void insert_character_test(uint64_t max_text_size, uint64_t number_of_insertion, uint64_t number_of_trials,
+                                          std::function<void(TEXT &, const std::vector<uint8_t> &, const std::vector<uint8_t> &)> builder_function, bool detailed_check, uint64_t seed, int message_paragraph = stool::Message::SHOW_MESSAGE)
         {
             std::cout << stool::Message::get_paragraph_string(message_paragraph) << "Insert Character Test: " << std::endl;
             for (uint64_t type = 0; type <= stool::UInt8VectorGenerator::get_max_alphabet_type(); type++)
             {
-
-                std::cout << stool::Message::get_paragraph_string(message_paragraph + 1) << "Alphabet Type: " << type << std::flush;
+                std::cout << stool::Message::get_paragraph_string(message_paragraph + 1) << "Alphabet Type: " << type << ", len = " << std::flush;
                 uint64_t len = 2;
-                std::vector<uint8_t> chars = stool::UInt8VectorGenerator::create_alphabet(type);
+                std::vector<uint8_t> alphabet = stool::UInt8VectorGenerator::create_alphabet(type);
 
-                while (len < number_of_insertion)
+
+                while (len < max_text_size)
                 {
-                    std::cout << "+" << std::flush;
-                    insert_character_test(test_text, chars, len, seed++);
+                    std::cout << len << " " << std::flush;
+
+                    for (uint64_t i = 0; i < number_of_trials; i++)
+                    {
+                        std::vector<uint8_t> _text = stool::UInt8VectorGenerator::create_random_sequence(len, alphabet, seed++);
+                        stool::NaiveDynamicString naive_text;
+                        for (char c : _text)
+                        {
+                            naive_text.push_back(c);
+                        }
+                        TEXT test_text;
+                        builder_function(test_text, _text, alphabet);
+
+                        insert_character_test(test_text, naive_text, alphabet, number_of_insertion, detailed_check, seed++);
+                    }
                     len *= 2;
                 }
                 std::cout << std::endl;
@@ -64,56 +80,66 @@ namespace stool
         }
 
         template <typename TEXT>
-        static void remove_character_test(TEXT &test_text, const std::vector<uint8_t> &alphabet, uint64_t text_size, uint64_t seed)
+        static void remove_character_test(TEXT &test_text, stool::NaiveDynamicString &naive_text, uint64_t number_of_removals, bool detailed_check, uint64_t seed)
         {
             std::mt19937_64 mt64(seed);
             std::uniform_int_distribution<uint64_t> get_rand_uni_int(0, UINT64_MAX);
 
-            std::vector<uint8_t> _text = stool::UInt8VectorGenerator::create_random_sequence(text_size, alphabet, seed);
-
-            test_text.clear();
-            test_text.build_from_text(_text, alphabet);
-
-            stool::NaiveDynamicString naive_text;
-            for (char c : _text)
-            {
-                naive_text.push_back(c);
-            }
-
-            while (naive_text.size() > 0)
-            {
+            for(uint64_t i = 0; i < number_of_removals; i++){
                 uint64_t nth = get_rand_uni_int(mt64) % (naive_text.size());
                 naive_text.remove(nth);
                 test_text.remove(nth);
 
-                if (naive_text.size() % 100 == 0)
-                {
+                assert((uint64_t)test_text.size() == (uint64_t)naive_text.size());
+                if(naive_text.size() == 0){
+                    break;
+                }
+                if(detailed_check){
                     std::string test_str = test_text.to_string();
                     std::string naive_str = naive_text.to_string();
-                    stool::EqualChecker::equal_check(test_str, naive_str);
+                    stool::EqualChecker::equal_check(test_str, naive_str);        
                 }
             }
 
-            if (test_text.size() != 0)
+
+            if ((int64_t)test_text.size() != (int64_t)naive_text.size())
             {
-                std::cout << "Error: test_text.size() != 0" << std::endl;
+                throw std::runtime_error("Error: test_text.size() != naive_text.size()");
             }
+
+            std::string test_str = test_text.to_string();
+            std::string naive_str = naive_text.to_string();
+            stool::EqualChecker::equal_check(test_str, naive_str);
         }
 
         template <typename TEXT>
-        static void remove_character_test(TEXT &test_text, uint64_t max_text_size, uint64_t seed, int message_paragraph = stool::Message::SHOW_MESSAGE)
+        static void remove_character_test(uint64_t max_text_size, uint64_t number_of_removals, uint64_t number_of_trials, std::function<void(TEXT &, const std::vector<uint8_t> &, const std::vector<uint8_t> &)> builder_function, bool detailed_check, uint64_t seed, int message_paragraph = stool::Message::SHOW_MESSAGE)
         {
             std::cout << stool::Message::get_paragraph_string(message_paragraph) << "Remove Character Test: " << std::endl;
             for (uint64_t type = 0; type <= stool::UInt8VectorGenerator::get_max_alphabet_type(); type++)
             {
-                std::cout << stool::Message::get_paragraph_string(message_paragraph + 1) << "Alphabet Type: " << type << std::flush;
+                std::cout << stool::Message::get_paragraph_string(message_paragraph + 1) << "Alphabet Type: " << type << ", len = " << std::flush;
                 uint64_t len = 2;
-                std::vector<uint8_t> chars = stool::UInt8VectorGenerator::create_alphabet(type);
+                std::vector<uint8_t> alphabet = stool::UInt8VectorGenerator::create_alphabet(type);
+
 
                 while (len < max_text_size)
                 {
-                    std::cout << "+" << std::flush;
-                    remove_character_test(test_text, chars, len, seed++);
+                    std::cout << len << " " << std::flush;
+
+                    for (uint64_t i = 0; i < number_of_trials; i++)
+                    {
+                        std::vector<uint8_t> _text = stool::UInt8VectorGenerator::create_random_sequence(len, alphabet, seed++);
+                        stool::NaiveDynamicString naive_text;
+                        for (char c : _text)
+                        {
+                            naive_text.push_back(c);
+                        }
+                        TEXT test_text;
+                        builder_function(test_text, _text, alphabet);
+
+                        remove_character_test(test_text, naive_text, number_of_removals, detailed_check, seed++);
+                    }
                     len *= 2;
                 }
                 std::cout << std::endl;
@@ -121,7 +147,69 @@ namespace stool
             std::cout << "[DONE]" << std::endl;
         }
 
-        
+        template <typename TEXT>
+        static void random_test(uint64_t text_size, uint64_t alphabet_type, uint64_t number_of_queries, uint64_t number_of_trials, 
+            std::function<void(TEXT &, const std::vector<uint8_t> &, const std::vector<uint8_t> &)> builder_function, bool detailed_check, uint64_t seed, [[maybe_unused]] int message_paragraph = stool::Message::SHOW_MESSAGE)
+        {
+
+            std::cout << "RANDOM_TEST: \t" << std::endl;
+            std::mt19937 mt(seed);
+            std::uniform_int_distribution<uint64_t> get_rand_value(0, UINT64_MAX);
+
+            for (uint64_t i = 0; i < number_of_trials; i++)
+            {
+                std::cout << stool::Message::get_paragraph_string(message_paragraph+1) << i << ": " << std::flush;
+                uint64_t counter = 0;
+                std::vector<uint8_t> alphabet = stool::UInt8VectorGenerator::create_alphabet(alphabet_type);
+                std::vector<uint8_t> _text = stool::UInt8VectorGenerator::create_random_sequence(text_size, alphabet, seed++);
+                stool::NaiveDynamicString naive_text;
+                for (char c : _text)
+                {
+                    naive_text.push_back(c);
+                }
+                TEXT test_text;
+                builder_function(test_text, _text, alphabet);
+
+                while (counter < number_of_queries)
+                {
+                    std::cout << "+" << std::flush;
+
+                    uint64_t type = get_rand_value(mt) % 6;
+                    if (type == 0 || type == 1)
+                    {
+                        insert_character_test(test_text, naive_text, alphabet, 5, detailed_check, seed++);
+                    }
+                    else if (type == 2 || type == 3)
+                    {
+                        remove_character_test(test_text, naive_text, 1, detailed_check, seed++);
+                    }
+                    else if (type == 4)
+                    {
+                        stool::StringTest::access_character_test(test_text, naive_text.text, 5, seed++);
+                    }
+                    else
+                    {
+                        stool::StringTest::locate_query_test(test_text, naive_text.text, 5, 100, seed++);
+                    }
+                    counter++;
+
+                    if (detailed_check)
+                    {
+                        std::string test_str = test_text.to_string();
+                        std::string naive_str = naive_text.to_string();
+                        stool::EqualChecker::equal_check(test_str, naive_str);
+                    }
+                }
+
+                std::string test_str = test_text.to_string();
+                std::string naive_str = naive_text.to_string();
+                stool::EqualChecker::equal_check(test_str, naive_str);
+
+                std::cout << std::endl;
+            }
+            std::cout << "[DONE]" << std::endl;
+        }
+
     };
 
 } // namespace name
