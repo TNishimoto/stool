8bb9ff86ce66dd75cf206fc93464b6ff7f699952
Nishimoto <a32b16c4@gmail.com>
2026-07-21 16:00:55 +0900

    experiment

--- experiment/all.sh
+++ experiment/all.sh
@@ -4,7 +4,7 @@
 dataset_folder_path=$1
 output_folder_path=$2
 
-nohup bash sa.sh $1 $2 &
-nohup bash dsa.sh $1 $2 &
-nohup bash delta.sh $1 $2 &
-nohup bash dsa_delta.sh $1 $2 &
+nohup bash sa.sh $1 $2 >> $2/error.log 2>&1
+nohup bash dsa.sh $1 $2 >> $2/error.log 2>&1
+nohup bash delta.sh $1 $2 >> $2/error.log 2>&1
+nohup bash dsa_delta.sh $1 $2 >> $2/error.log 2>&1
